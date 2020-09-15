f0724b1e6e12933bca5a05821c5d790a8d7b5e64
mmdaozhu <bin19920216@gmail.com>
2020-09-15 16:37:37 +0800

    mend 041

--- cpp/041.FirstMissingPositive/FirstMissingPositive.cpp
+++ cpp/041.FirstMissingPositive/FirstMissingPositive.cpp
@@ -1,28 +1,28 @@
 /*
 解体思路：
-    申请一个size比一组数据个数大1的数组array。
+    申请一个size比这组数据个数大1的数组array。
     迭代这组数据，忽略非正整数。如果数据元素值小于数组array的size，以自身为index塞到数组array中，并留下标志。
-    最好迭代数组array，找到第一个没有标志的元素，并返回index。	
+    最后迭代数组array，找到第一个没有标志的元素，并返回index。
 
 时间复杂度分析：O(n)
 */
 
 #include <assert.h>
 
+#include <array>
 #include <iostream>
 #include <vector>
 
 class Solution {
 public:
     int firstMissingPositive(std::vector<int>& nums) {
-        int size = nums.size();
-        std::vector<int> array(size + 1);
-        for (unsigned int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0 && nums[i] < array.size()) {
-                array[nums[i]] = 1;
+        std::vector<int> array(nums.size() + 1);
+        for (const auto& n : nums) {
+            if (n > 0 && n < array.size()) {
+                array[n] = 1;
             }
         }
-        for (unsigned int i = 1; i < array.size(); i++) {
+        for (auto i = 1; i < array.size(); i++) {
             if (array[i] == 0) {
                 return i;
             }
