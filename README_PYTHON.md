f392383c265f9cf2cdf6fd0fad410ee9de88c93a
mmdaozhu <bin19920216@gmail.com>
2020-12-03 10:54:08 +0800

    add python 206

--- README_PYTHON.md
+++ README_PYTHON.md
@@ -6,3 +6,4 @@ LeetCode
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
 |001|[Two Sum](https://leetcode.com/problems/two-sum/)| [Python](./python3/001.TwoSum/TwoSum.py)|Easy|
+|206|[Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)| [Python](./python3/206.ReverseLinkedList/ReverseLinkedList.py)|Easy|
---
+++ python3/206.ReverseLinkedList/ReverseLinkedList.py
@@ -0,0 +1,46 @@
+'''
+解体思路：
+    p(prev)
+    c(cur)
+    r(rear)
+            1   >   2   >   3   >   4   >   5   >   NULL
+    p       c
+    p       c   >   r   (第一步)
+    p   <   c   >   r   (第二步)
+            p   >   r   (第三步)
+        <   p   >   c   (第四步)
+
+时间复杂度分析：O(n)
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, cur = None, head
+        while(cur):
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
+
+
+def test1():
+    five = ListNode(5)
+    four = ListNode(4, five)
+    three = ListNode(3, four)
+    two = ListNode(2, three)
+    one = ListNode(1, two)
+
+    s = Solution()
+    rlist = s.reverseList(one)
+    while(rlist):
+        print(rlist.val)
+        rlist = rlist.next
+
+
+test1()
